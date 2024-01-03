2c0349f9f645bd4b3acbd4a32ecfb186f5caf91f
solidados <geekbrains.pk@internet.ru>
2024-01-03 23:03:29 +0400

    feat: install nodemon pkg to reload server in development mode

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ dotenv.config()
 const PORT = process.env.PORT
 const app = express()
 
+app.get('/', (req, res) => res.status(200).json('Server works...'))
+
 app.listen(PORT, ()=> console.log(`Server started on PORT: ${PORT}`))
 
 
