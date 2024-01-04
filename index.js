0f98c3641c4e1c427d01fac171b8110e7d71f26d
solidados <geekbrains.pk@internet.ru>
2024-01-04 21:27:41 +0400

    feat: register Router in index.js

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const router = require('./router/Router.js')
 
 const dotenv = require('dotenv')
 dotenv.config()
-
-const Post = require('./posts/post-schema')
 
 const PORT = process.env.PORT || 5000
 const MONGO_DB = process.env.MONGO_DB;
@@ -12,19 +11,7 @@ const MONGO_DB = process.env.MONGO_DB;
 const app = express()
 
 app.use(express.json())
-
-/** All requests to DB are async, so I add async..await
- * that requests to DB would not block the main Thread*/
-app.post('/', async (req, res) => {
-  try {
-    const {author, title, content, picture} = req.body;
-    const post = await Post.create({author, title, content, picture})
-    res.json(post)
-  }
-  catch (err) {
-    res.status(500).json(err)
-  }
-})
+app.use('/api', router)
 
 async function startApp () {
   try {
--- router/Router.js
+++ router/Router.js
@@ -1,8 +1,18 @@
 const Router = require('express')
+const Post = require("../posts/post-schema.js");
 
 const router = new Router()
 
-router.post('/posts')
+router.post('/posts', async (req, res) => {
+  try {
+    const {author, title, content, picture} = req.body;
+    const post = await Post.create({author, title, content, picture})
+    res.json(post)
+  }
+  catch (err) {
+    res.status(500).json(err)
+  }
+})
 router.get('/posts')
 router.get('/posts/:postId')
 router.put('/posts')
