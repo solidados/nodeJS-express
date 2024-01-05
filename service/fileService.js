9a2a531d95e0de22f4cb636c7b344c7822e9c16e
solidados <geekbrains.pk@internet.ru>
2024-01-05 12:28:23 +0400

    feat: import uuid and path into fileService and describe file creation

--- service/fileService.js
+++ service/fileService.js
@@ -1,6 +1,17 @@
+const uuid = require('uuid')
+const path = require('node:path')
+
 class FileService {
   async saveFile(file) {
-
+    try {
+      const fileName = `${uuid.v4()}.jpeg`
+      const filePath = path.resolve(__dirname, 'static', fileName)
+      await file.mv(filePath)
+      return fileName;
+    }
+    catch (err) {
+      console.error(err.message)
+    }
   }
 }
 
